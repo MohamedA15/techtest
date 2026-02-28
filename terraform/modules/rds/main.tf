30e497334be4ec9e4535085c96110137eb114d77
MohamedA15 <mo684073@gmail.com>
2026-02-28 17:16:04 +0000

    adding postgreSQL

---
+++ terraform/modules/rds/main.tf
@@ -0,0 +1,30 @@
+resource "aws_db_subnet_group" "this" {
+  name       = "rds-subnet-group"
+  subnet_ids = var.private_subnets
+}
+
+resource "aws_db_instance" "this" {
+  identifier            = "outline-db"
+  allocated_storage     = 20
+  max_allocated_storage = 100
+  storage_type          = "gp3"
+
+  engine         = "postgres"
+  engine_version = "15"
+  instance_class = var.db_instance_class
+  db_name        = var.db_name
+
+  username = var.db_username
+  password = var.db_password
+
+  publicly_accessible = false
+  skip_final_snapshot = true
+
+  multi_az = true
+
+  backup_retention_period = 7
+  storage_encrypted       = true
+
+  vpc_security_group_ids = [var.rds_sg_id]
+  db_subnet_group_name   = aws_db_subnet_group.this.name
+}
