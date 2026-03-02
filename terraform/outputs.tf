ca89089598f939b30dd6e5456ad9cad8d0a30a7a
MohamedA15 <mo684073@gmail.com>
2026-03-02 09:31:10 +0000

    fixing outputs

---
+++ terraform/outputs.tf
@@ -0,0 +1,22 @@
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.vpc.public_subnet_ids
+}
+
+output "private_subnet_ids" {
+  value = module.vpc.private_subnet_ids
+}
+
+
+output "alb_sg_id" {
+  value = module.security_groups.alb_sg_id
+}
+
+output "ecs_sg_id" {
+  value = module.security_groups.ecs_sg_id
+}
+
+
